d103f06c557cda2fd7ad1a2aa443d33441e18878
ZilvaX <Kevin.x.g@gmail.com>
2015-05-01 23:11:58 +1200

    roller rolls now
    Roller rolls if input argument is 20 now.

--- roller.java
+++ roller.java
@@ -3,6 +3,14 @@ package diceApp;
 public class roller {
 	public static void main(String[] args){
 		int diceNumber = Integer.parseInt(args[0]);
-		
+		int diceValue;
+		if (diceNumber == 20){
+			Dice rolled = new d20();
+			diceValue = rolled.roll();
+		} else {
+			System.out.println("Not a valid input");
+			return;
+		}
+		System.out.println(diceValue);
 	}
 }
