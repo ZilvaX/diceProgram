bd4b3479eb1f265b077b2a515f0aaee00a32c06f
ZilvaX <Kevin.x.g@gmail.com>
2015-05-01 23:16:51 +1200

    Rolls all D&D types of dice
    Can now roll d4 d6 d8 d10 d12 d20s

--- roller.java
+++ roller.java
@@ -7,7 +7,22 @@ public class roller {
 		if (diceNumber == 20){
 			Dice rolled = new d20();
 			diceValue = rolled.roll();
-		} else {
+		} else if(diceNumber == 12){
+			Dice rolled = new d12();
+			diceValue = rolled.roll();
+		} else if(diceNumber == 10){
+			Dice rolled = new d10();
+			diceValue = rolled.roll();
+		} else if(diceNumber == 8){
+			Dice rolled = new d8();
+			diceValue = rolled.roll();
+		} else if(diceNumber == 6){
+			Dice rolled = new d6();
+			diceValue = rolled.roll();
+		} else if(diceNumber == 4){
+			Dice rolled = new d4();
+			diceValue = rolled.roll();
+		}else {
 			System.out.println("Not a valid input");
 			return;
 		}
